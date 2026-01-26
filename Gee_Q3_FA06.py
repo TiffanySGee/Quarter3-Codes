220a6f995ce06b240b036c45dc1c7b997bbf0107
TiffanySGee <sophietiffany.gee@smc.pshs.edu.ph>
2026-01-26 17:18:12 +0800

    Create Gee_Q3_FA06.py

---
+++ Gee_Q3_FA06.py
@@ -0,0 +1,14 @@
+# Create an empty dictionary student = {}
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+subject = input("Enter your favorite subject: ")
+
+student["name"] = name
+student["age"] = age
+student["subject"] = subject
+
+print("\nStudent Record:")
+print("Name:", student["name"])
+print("Age:", student["age"])
+print("Favorite Subject:", student["subject"])
